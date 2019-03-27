2e4a3c1df761c94a860b541506b52c77751eff59
Louise Findlay <louisefindlay23@outlook.com>
2019-03-27 13:34:14 +0000

    Modified show/hide arrows for testimonials so layout remains centered

--- js/script.js
+++ js/script.js
@@ -5,12 +5,16 @@ $(document).ready(function() {
 	
     $("#silvernotegrey.testimonial, .bbc.testimonial, .dragon.testimonial, #gold.testimonial").hide();
 	
+	$(".fa-chevron-left").addClass("hidden");
+	
 	$(".webslide1").click(function(){
 		
 		$("#silvernotegrey.testimonial, .bbc.testimonial").hide();
 		$(".nescol.testimonial").show();
 		$(".dots .webslide2, .dots .webslide3").removeClass("orange");
 		$(".dots .webslide1").addClass("orange");
+		$(".fa-chevron-left").addClass("hidden");
+		$(".fa-chevron-right").removeClass("hidden");
 });
 	
 	$(".webslide2").click(function(){
@@ -19,6 +23,8 @@ $(document).ready(function() {
 		$("#silvernotegrey.testimonial").show();
 		$(".dots .webslide1, .dots .webslide3").removeClass("orange");
 		$(".dots .webslide2").addClass("orange");
+		$(".fa-chevron-left").removeClass("hidden");
+		$(".fa-chevron-right").removeClass("hidden");
 });
 	
 	$(".webslide3").click(function(){
@@ -27,6 +33,8 @@ $(document).ready(function() {
 		$(".bbc.testimonial").show();
 		$(".dots .webslide1, .dots .webslide2").removeClass("orange");
 		$(".dots .webslide3").addClass("orange");
+		$(".fa-chevron-right").addClass("hidden");
+		$(".fa-chevron-left").removeClass("hidden");
 });
 	
 	$(".authorslide1").click(function(){
@@ -35,6 +43,8 @@ $(document).ready(function() {
 		$("#lavender.testimonial").show();
 		$(".dots .authorslide2, .dots .authorslide3").removeClass("orange");
 		$(".dots .authorslide1").addClass("orange");
+		$(".fa-chevron-left").addClass("hidden");
+		$(".fa-chevron-right").removeClass("hidden");
 	});
 	
 	$(".authorslide2").click(function(){
@@ -43,6 +53,8 @@ $(document).ready(function() {
 		$("#gold.testimonial").show();
 		$(".dots .authorslide1, .dots .authorslide3").removeClass("orange");
 		$(".dots .authorslide2").addClass("orange");
+		$(".fa-chevron-left").removeClass("hidden");
+		$(".fa-chevron-right").removeClass("hidden");
 	});
 	
 	$(".authorslide3").click(function(){
@@ -51,5 +63,7 @@ $(document).ready(function() {
 		$(".dragon.testimonial").show();
 		$(".dots .authorslide1, .dots .authorslide2").removeClass("orange");
 		$(".dots .authorslide3").addClass("orange");
+		$(".fa-chevron-right").addClass("hidden");
+		$(".fa-chevron-left").removeClass("hidden");
 	});
 });

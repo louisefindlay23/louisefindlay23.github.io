7128e3d5e2d96de55fe6dcb7865d0514d991d3f1
louisefindlay23 <louisefindlay23@outlook.com>
2020-05-05 18:41:13 +0100

    Changed testimonial order

--- js/script.js
+++ js/script.js
@@ -3,58 +3,49 @@
 
 $(document).ready(function () {
 
-    $("#test2, #test3, #test4, #test5, #test6").hide();
+    $("#test2, #test3, #test4, #test5").hide();
     $(".fa-chevron-left").addClass("hidden");
 
     // Testimonial Controls
     $(".slide1").click(function () {
-        $("#test2, #test3, #test4, #test5, #test6").hide();
+        $("#test2, #test3, #test4, #test5").hide();
         $("#test1").show();
-        $(".dots .slide2, .dots .slide3, .dots .slide4, .dots .slide5, .dots .slide6").removeClass("orange");
+        $(".dots .slide2, .dots .slide3, .dots .slide4, .dots .slide5").removeClass("orange");
         $(".dots .slide1").addClass("orange");
         $(".fa-chevron-left").addClass("hidden");
         $(".fa-chevron-right").removeClass("hidden");
     });
 
     $(".slide2").click(function () {
-        $("#test1, #test3, #test4, #test5, #test6").hide();
+        $("#test1, #test3, #test4, #test5").hide();
         $("#test2").show();
-        $(".dots .slide1, .dots .slide3, .dots .slide4, .dots .slide5, .dots .slide6").removeClass("orange");
+        $(".dots .slide1, .dots .slide3, .dots .slide4, .dots .slide5").removeClass("orange");
         $(".dots .slide2").addClass("orange");
         $(".fa-chevron-left, .fa-chevron-right").removeClass("hidden");
     });
 
     $(".slide3").click(function () {
-        $("#test1, #test2, #test4, #test5, #test6").hide();
+        $("#test1, #test2, #test4, #test5").hide();
         $("#test3").show();
-        $(".dots .slide1, .dots .slide2, .dots .slide4, .dots .slide5, .dots .slide6").removeClass("orange");
+        $(".dots .slide1, .dots .slide2, .dots .slide4, .dots .slide5").removeClass("orange");
         $(".dots .slide3").addClass("orange");
         $(".fa-chevron-left, .fa-chevron-right").removeClass("hidden");
     });
 
     $(".slide4").click(function () {
-        $("#test1, #test2, #test3, #test5, #test6").hide();
+        $("#test1, #test2, #test3, #test5").hide();
         $("#test4").show();
-        $(".dots .slide1, .dots .slide2, .dots .slide3, .dots .slide5, .dots .slide6").removeClass("orange");
+        $(".dots .slide1, .dots .slide2, .dots .slide3, .dots .slide5").removeClass("orange");
         $(".dots .slide4").addClass("orange");
         $(".fa-chevron-left, .fa-chevron-right").removeClass("hidden");
     });
 
     $(".slide5").click(function () {
-        $("#test1, #test2, #test3, #test4, #test6").hide();
+        $("#test1, #test2, #test3, #test4").hide();
         $("#test5").show();
-        $(".dots .slide1, .dots .slide2, .dots .slide3, .dots .slide4, .dots .slide6").removeClass("orange");
+        $(".dots .slide1, .dots .slide2, .dots .slide3, .dots .slide4").removeClass("orange");
         $(".dots .slide5").addClass("orange");
         $(".fa-chevron-left, .fa-chevron-right").removeClass("hidden");
-    });
-
-    $(".slide6").click(function () {
-        $("#test1, #test2, #test3, #test4, #test5").hide();
-        $("#test6").show();
-        $(".dots .slide1, .dots .slide2, .dots .slide3, .dots .slide4, .dots .slide5").removeClass("orange");
-        $(".dots .slide6").addClass("orange");
-        $(".fa-chevron-right").addClass("hidden");
-        $(".fa-chevron-left").removeClass("hidden");
     });
 
     // Animation when element is in view

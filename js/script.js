8e62b30c7d0d783be193c62cc8b229420a9756cd
louisefindlay23 <louisefindlay23@outlook.com>
2020-05-05 17:50:39 +0100

    Added Jane's testimonial

--- js/script.js
+++ js/script.js
@@ -3,40 +3,48 @@
 
 $(document).ready(function () {
 
-    $("#test2, #test3, #test4").hide();
+    $("#test2, #test3, #test4, #test5").hide();
     $(".fa-chevron-left").addClass("hidden");
 
     // Testimonial Controls
     $(".slide1").click(function () {
-        $("#test2, #test3, #test4").hide();
+        $("#test2, #test3, #test4, #test5").hide();
         $("#test1").show();
-        $(".dots .slide2, .dots .slide3, .dots .slide4").removeClass("orange");
+        $(".dots .slide2, .dots .slide3, .dots .slide4, .dots .slide5").removeClass("orange");
         $(".dots .slide1").addClass("orange");
         $(".fa-chevron-left").addClass("hidden");
         $(".fa-chevron-right").removeClass("hidden");
     });
 
     $(".slide2").click(function () {
-        $("#test1, #test3, #test4").hide();
+        $("#test1, #test3, #test4, #test5").hide();
         $("#test2").show();
-        $(".dots .slide1, .dots .slide3, .dots .slide4").removeClass("orange");
+        $(".dots .slide1, .dots .slide3, .dots .slide4, .dots .slide5").removeClass("orange");
         $(".dots .slide2").addClass("orange");
         $(".fa-chevron-left, .fa-chevron-right").removeClass("hidden");
     });
 
     $(".slide3").click(function () {
-        $("#test1, #test2, #test4").hide();
+        $("#test1, #test2, #test4, #test5").hide();
         $("#test3").show();
-        $(".dots .slide1, .dots .slide2, .dots .slide4").removeClass("orange");
+        $(".dots .slide1, .dots .slide2, .dots .slide4, .dots .slide5").removeClass("orange");
         $(".dots .slide3").addClass("orange");
         $(".fa-chevron-left, .fa-chevron-right").removeClass("hidden");
     });
 
     $(".slide4").click(function () {
-        $("#test1, #test2, #test3").hide();
+        $("#test1, #test2, #test3, #test5").hide();
         $("#test4").show();
-        $(".dots .slide1, .dots .slide2, .dots .slide3").removeClass("orange");
+        $(".dots .slide1, .dots .slide2, .dots .slide3, .dots .slide5").removeClass("orange");
         $(".dots .slide4").addClass("orange");
+        $(".fa-chevron-left, .fa-chevron-right").removeClass("hidden");
+    });
+
+    $(".slide5").click(function () {
+        $("#test1, #test2, #test3, #test4").hide();
+        $("#test5").show();
+        $(".dots .slide1, .dots .slide2, .dots .slide3, .dots .slide4").removeClass("orange");
+        $(".dots .slide5").addClass("orange");
         $(".fa-chevron-right").addClass("hidden");
         $(".fa-chevron-left").removeClass("hidden");
     });

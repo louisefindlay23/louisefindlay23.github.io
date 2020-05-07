53f384d666d8c1097c1f73f03a5190237ce15fbd
louisefindlay23 <louisefindlay23@outlook.com>
2020-05-07 20:25:33 +0100

    Added Lightbox code

--- js/script.js
+++ js/script.js
@@ -109,4 +109,14 @@ $(document).ready(function () {
         }
     });
 
+    // Lightbox
+
+    var pictures = document.querySelectorAll('picture');
+
+    pictures.forEach(function (pic) {
+        pic.addEventListener('click', function (e) {
+            this.classList.toggle('lightbox');
+        });
+    });
+
 });

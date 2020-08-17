7210cf509658e48a8b7305c1bfa17ac624b74e19
louisefindlay23 <louise@louisefindlay.com>
2020-08-17 17:47:04 +0100

    Started to refactor person testimonial code

--- js/script.js
+++ js/script.js
@@ -4,39 +4,7 @@
 $(document).ready(function () {
 
     // Testimonial Controls
-    $(".slide1").click(function () {
-        $("#test2, #test3, #test4, #test5").hide();
-        $("#test1").show();
-        $(".dots .slide2, .dots .slide3, .dots .slide4, .dots .slide5").removeClass("orange");
-        $(".dots .slide1").addClass("orange");
-        $(".fa-chevron-left").addClass("hidden");
-        $(".fa-chevron-right").removeClass("hidden");
-    });
-
-    $(".slide2").click(function () {
-        $("#test1, #test3, #test4, #test5").hide();
-        $("#test2").show();
-        $(".dots .slide1, .dots .slide3, .dots .slide4, .dots .slide5").removeClass("orange");
-        $(".dots .slide2").addClass("orange");
-        $(".fa-chevron-left, .fa-chevron-right").removeClass("hidden");
-    });
-
-    $(".slide3").click(function () {
-        $("#test1, #test2, #test4, #test5").hide();
-        $("#test3").show();
-        $(".dots .slide1, .dots .slide2, .dots .slide4, .dots .slide5").removeClass("orange");
-        $(".dots .slide3").addClass("orange");
-        $(".fa-chevron-left, .fa-chevron-right").removeClass("hidden");
-    });
-
-    $(".slide4").click(function () {
-        $("#test1, #test2, #test3, #test5").hide();
-        $("#test4").show();
-        $(".dots .slide1, .dots .slide2, .dots .slide3, .dots .slide5").removeClass("orange");
-        $(".dots .slide4").addClass("orange");
-        $(".fa-chevron-left").removeClass("hidden");
-        $(".fa-chevron-right").addClass("hidden");
-    });
+    if ()
 
     // Animation when element is in view
     $('#development').on('inview', function (event, isInView) {
